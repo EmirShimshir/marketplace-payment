c977ef25f4163e7df33b8952cf1b1ac62a769b47
Shimshir Emirdzan IU7-23B <shimshireao@student.bmstu.ru>
2024-05-17 19:34:44 +0300

    use payment lib

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/EmirShimshir/marketplace-payment
+
+go 1.22.2
+
+require github.com/EmirShimshir/marketplace-domain v0.0.0-20240517151020-6fd8bd582f8d
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/guregu/null v4.0.0+incompatible // indirect
+)
---
+++ payment/mock/paymentMock.go
@@ -0,0 +1,40 @@
+package paymentMock
+
+import (
+	"context"
+	"fmt"
+	"github.com/EmirShimshir/marketplace-domain/domain"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+type MockGateway struct {
+}
+
+func NewtMockGateway() *MockGateway {
+	return &MockGateway{}
+}
+
+func (g *MockGateway) GetPaymentUrl(ctx context.Context, payload domain.PaymentPayload) (url.URL, error) {
+	return url.URL{
+		Scheme: "https",
+		Host:   "MockGateway.ru",
+		Path:   fmt.Sprintf("%s&%s", payload.OrderID, strconv.Itoa(int(payload.PaySum))),
+	}, nil
+}
+
+func (g *MockGateway) ProcessPayment(ctx context.Context, key string) (domain.PaymentPayload, error) {
+	res := strings.Split(key, "&")
+	if len(res) != 2 {
+		return domain.PaymentPayload{}, domain.ErrInvalidPaymentSum
+	}
+	sum, err := strconv.ParseInt(res[1], 10, 64)
+	if err != nil {
+		return domain.PaymentPayload{}, err
+	}
+	return domain.PaymentPayload{
+		OrderID: domain.ID(res[0]),
+		PaySum:  sum,
+	}, nil
+}
